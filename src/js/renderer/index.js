8c84bb9c4efda8da831cdeb7c8d311e386d114f1
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-04-08 20:27:28 +0900

    Setup application base

---
+++ src/js/renderer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'photon/dist/css/photon.css'
+
+ReactDOM.render(
+  <div>Hello Electron!</div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const mainConfig = {
+  target: 'electron-main',
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  entry: { main: __dirname + '/src/js/main/index.js' },
+  output: { path: __dirname + '/app', filename: '[name].js' },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        options: {
+          fix: true,
+          failOnError: false,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
+      },
+      {
+        test:   /\.(png|jpg|ttf|otf|eot|svg|woff2?)(\?.+)?$/,
+        loader: 'url-loader',
+        options:  {
+          limit: 30000
+        }
+      },
+    ],
+  },
+}
+
+const rendererConfig = {
+  target: 'electron-renderer',
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  entry: { renderer: __dirname + '/src/js/renderer/index.js' },
+  output: { path: __dirname + '/app', filename: '[name].js' },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        options: {
+          fix: true,
+          failOnError: false,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
+      },
+      {
+        test:   /\.(png|jpg|ttf|otf|eot|svg|woff2?)(\?.+)?$/,
+        loader: 'url-loader',
+        options:  {
+          limit: 30000
+        }
+      },
+    ],
+  },
+}
+
+module.exports = [ mainConfig, rendererConfig ]
